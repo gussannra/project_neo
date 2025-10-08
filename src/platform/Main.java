8e32e18f7ac4ee6c21553d4ecd9722d33edceb22
Sanna Ranta <sranta39@gmail.com>
2025-10-08 21:45:05 +0200

    introduce double buffering

--- src/platform/Main.java
+++ src/platform/Main.java
@@ -6,5 +6,23 @@ public class Main {
         WindowManager windowManager = new WindowManager("my first window", 512, 512);
         windowManager.createWindow();
 
+        int x = 0;
+
+        int[] pixels = new int[512 * 512];
+
+        while (true) {
+            windowManager.clearBuffer();
+
+            // pixels[x] = 
+
+            x += 0.5;
+
+            if (x >= 512) {
+                x = 0;
+            }
+
+            windowManager.flip(pixels);
+        }
+
     }
 }
--- src/platform/WindowManager.java
+++ src/platform/WindowManager.java
@@ -1,19 +1,32 @@
 package platform;
 
 import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.Color;
 import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 public class WindowManager extends WindowAdapter {
     private String title;
     private int width;
     private int height;
     private Frame frame;
+    private BufferStrategy bufferStrategy;
+    private BufferedImage buffer;
+    private Graphics2D bufferGraphics;
 
     public WindowManager(String title, int width, int height) {
         this.title = title;
         this.width = width;
         this.height = height;
         frame = null;
+        bufferStrategy = null;
+        buffer = new BufferedImage(512, 512, BufferedImage.TYPE_INT_RGB);
+        bufferGraphics = buffer.createGraphics();
+        bufferGraphics.setBackground(Color.BLACK);
     }
 
     public void createWindow() {
@@ -21,5 +34,28 @@ public class WindowManager extends WindowAdapter {
         frame.setVisible(true);
         frame.setSize(width, height);
         frame.setLocationRelativeTo(null);
+        frame.addWindowListener(this);
+
+        frame.createBufferStrategy(2);
+        bufferStrategy = frame.getBufferStrategy();
     }
+
+    public void flip(int[] pixels) {
+        buffer.setRGB(0, 0, 512, 512, pixels, 0, 512);
+        Graphics g = bufferStrategy.getDrawGraphics();
+        g.drawImage(buffer, 0, 0, frame.getWidth(), frame.getHeight(), null);
+        g.dispose();
+
+        bufferStrategy.show();
+    }
+
+    public void clearBuffer() {
+        bufferGraphics.clearRect(0, 0, frame.getWidth(), frame.getHeight());
+    }
+
+    @Override
+    public void windowClosing(WindowEvent e) {
+        frame.dispose();
+    }
+
 }
